f373e2e30bf4169cb69ef279830fead0b63af131
alexrodriguesbash <alek.rodrigues@hotmail.com>
2022-01-14 10:18:42 -0300

    useState, useEffect

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,18 +1,27 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 import { BASE_URL } from "utils/requests";
 
 function Listing() {
 
-    // FORMA ERRADA
-    axios.get(`${BASE_URL}/movies?size=12&page=0`)
-        .then(response => {
-            console.log(response.data);
-        });
+    const [pageNumber, setPagaNumber] = useState(0);
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/movies?size=12&page=1`)
+            .then(response => {
+                const data = response.data as MoviePage;
+                console.log(data); 
+                setPagaNumber(data.number);
+            });
+    }, []);
 
     return (
         <>
+            <p>{pageNumber}</p>
+
             <Pagination />
 
             <div className="container">
@@ -23,18 +32,18 @@ function Listing() {
                     <div className="col-sm-6 col-lg-4 col-xl-3  mb-3">
                         <MovieCard />
                     </div>
-                   
+
                     <div className="col-sm-6 col-lg-4 col-xl-3  mb-3">
                         <MovieCard />
                     </div>
-                   
+
                     <div className="col-sm-6 col-lg-4 col-xl-3  mb-3">
                         <MovieCard />
                     </div>
-                   
+
                     <div className="col-sm-6 col-lg-4 col-xl-3  mb-3">
                         <MovieCard />
-                    </div>  
+                    </div>
                 </div>
             </div>
         </>
